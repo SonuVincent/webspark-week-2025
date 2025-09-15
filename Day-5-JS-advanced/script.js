a9462ffc078dfa85b52687bdf87835dc1c87c748
Vikash Kumar <vikashjjp728@gmail.com>
2025-09-15 17:30:47 +0530

    Publish the workshop class source code and assignments.

---
+++ Day-5-JS-advanced/script.js
@@ -0,0 +1,41 @@
+// // let documents = document;
+// // console.log(documents)
+// // console.log(documents.getElementById("body-id"))
+
+// // documents.getElementById("body-id").style.backgroundColor="red"
+
+// // let addButton = documents.getElementById("add-button")
+// // addButton.addEventListener("click", (event) => {
+// //     alert("Event")
+// // })
+
+
+// let newArray = [];
+// console.log(newArray);
+
+// newArray.push("hello")
+// newArray.push("hello1")
+
+// // console.log(newArray);
+// // newArray.pop();
+// console.log(newArray);
+
+// newArray.forEach((value, index) => {
+//     value = "Hi" + index
+//     console.log(value, "==>", index);
+// })
+
+// newArray.map((value, index) => {
+//     value = "Hi" + index
+//     console.log(value, "==>", index);
+// })
+
+
+// console.log(newArray);
+
+
+
+let obj = {
+    name: "Vikash"
+}
+console.log(obj)
